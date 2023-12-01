4b1a7106b75aecfe775e5cd2a6e765cf0419b720
Dylan <dylan.p@tiflabs.in>
2023-12-01 14:48:45 -0500

    >No longer waits for console flag >Waits on input not being disabled anymore

--- index.js
+++ index.js
@@ -1,80 +1,86 @@
 const puppeteer = require('puppeteer');
 
-async function typeSlowly(page, selector, text, delay) {
-	for (const char of text) {
-		await page.type(selector, char);
-		const timeout = getRandomInt(50, 175)
-		console.log('timeout: ', timeout);
-		await page.waitForTimeout(timeout);
-	}
-}
-
-async function typeracer(string) {
-	try {
-		const browser = await puppeteer.launch({
-			headless: false,
-			args: [
-				'--start-maximized',
-				"--enable-automation"
-			],
-			devtools: true,
-			defaultViewport: null,
-		});
-		// Create a new page
-		const page = await browser.newPage();
-		// Navigate to the Typeracer game page
-		await page.goto('https://play.typeracer.com');
-
-		await page.waitForFunction(() => {
-			return document.querySelector("table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(1) > td > div > div")?.innerText?.length
-		}, { timeout: 0 })
-		const text = await page.evaluate(() => {
-			return document.querySelector("table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(1) > td > div > div")?.innerText
-		})
-		console.log('text: ', text);
-		await nowDoIt(page, text)
-
-	} catch (error) {
-		console.log('error: ', error);
-
-	}
-}
-
-function getRandomInt(min, max) {
+// Helper function to get a random integer within a range
+const getRandomInt = (min, max) => {
 	min = Math.ceil(min);
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
-const nowDoIt = async (page) => {
-	await page.waitForFunction(() => {
-		return document.querySelector("table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(1) > td > div > div")?.innerText?.length
-	}, { timeout: 0 })
-	const text = await page.evaluate(() => {
-		return document.querySelector("table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(1) > td > div > div")?.innerText
-	})
-	console.log('text: ', text);
-	await page.waitForFunction(() => {
-		try {
-			if (test) {
-				console.log('test: ', test);
-				return test
-			}
-		} catch (error) {
+// Function to type characters into a page element with a delay
+const typeSlowly = async (page, selector, text, delay) => {
+	for (const char of text) {
+		await page.type(selector, char);
+		const timeout = getRandomInt(50, 175);
+		await page.waitForTimeout(timeout);
+	}
+};
 
-		}
-	}, { timeout: 0 });
-
-	console.log("hey");
-	await typeSlowly(
-		page,
-		"table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(2) > td > input",
-		text,
-		getRandomInt(70, 100)
+// Function to perform the typing operation on a Typeracer page
+const typingOperation = async (page, text) => {
+	// Wait for the text box to be present
+	await page.waitForFunction(
+		() => {
+			return (
+				document.querySelector(
+					'table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(1) > td > div > div',
+				)?.innerText?.length && !document.querySelector('table input').disabled
+			);
+		},
+		{ timeout: 0 },
 	);
-	await page.evaluate(() => {
-		test = false
-	})
-}
 
-typeracer('This is a test string.');
+	// Type the text slowly into the input field
+	const inputSelector =
+		'table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(2) > td > input';
+	await typeSlowly(page, inputSelector, text, getRandomInt(70, 100));
+
+	// Reset the test variable
+	await page.evaluate(() => {
+		test = false;
+	});
+};
+
+// Main function to automate Typeracer
+const typeracer = async () => {
+	try {
+		// Launch Puppeteer browser
+		const browser = await puppeteer.launch({
+			headless: false,
+			args: ['--start-maximized', '--enable-automation'],
+			devtools: true,
+			defaultViewport: null,
+		});
+
+		// Create a new page and navigate to Typeracer
+		const page = await browser.newPage();
+		await page.goto('https://play.typeracer.com');
+
+		// Wait for the text box to be present
+		await page.waitForFunction(
+			() => {
+				return document.querySelector(
+					'table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(1) > td > div > div',
+				)?.innerText?.length;
+			},
+			{ timeout: 0 },
+		);
+
+		// Extract text from the Typeracer page
+		const text = await page.evaluate(() => {
+			return document.querySelector(
+				'table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(1) > td > div > div',
+			)?.innerText;
+		});
+
+		console.log('Found text box:', text);
+
+		// Perform the typing operation
+		await typingOperation(page, text);
+	} catch (error) {
+		console.log('Error:', error);
+	}
+};
+
+// Example usage
+typeracer();
